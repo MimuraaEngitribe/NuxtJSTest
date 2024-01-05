b94ac7d733d92b8ebcc12f3960b03ac0041ddef3
MimuraaEngitribe <152856296+MimuraaEngitribe@users.noreply.github.com>
2024-01-06 05:48:48 +0900

    フロントエンド作成 (暫定)

---
+++ app.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const message = ref<string>("Hello world!");
+</script>
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>トップページ</h1>
+    <hr />
+  </div>
+</template>
