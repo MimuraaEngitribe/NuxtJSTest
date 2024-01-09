2efa32c82e1efb599e7cc268763955c5d0965d32
64-21427 <xd8bda3fd7n2m@gmail.com>
2024-01-10 07:53:38 +0900

    データベース情報取得・表示ロジック追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [],
+  modules: [
+    '@nuxtjs/apollo',
+  ],
   devtools: { enabled: true },
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'YOUR_GRAPHQL_ENDPOINT',
+      },
+    },
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
 <template>
   <div>
-    <h1>トップページ</h1>
-    <hr />
+    <ul>
+      <li v-for="company in companies" :key="company.id">
+        {{ company.name }} (ID: {{ company.id }})
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  data() {
+    return {
+      companies: [],
+    };
+  },
+  apollo: {
+    companies: {
+      query: gql`{
+        companies {
+          name
+          id
+        }
+      }`,
+    },
+  },
+};
+</script>
